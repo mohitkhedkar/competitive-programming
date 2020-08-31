9b91fdf36a226c86505e8596b362fa6864684596
mohitkhedkar <mohitkhedkar521@gmail.com>
2020-08-31 22:30:17 +0530

    loops

---
+++ Python/Arithmetic operators.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+add = a + b
+print(add)
+
+sub = a-b
+print(sub)
+
+mul = a*b
+print(mul)
---
+++ Python/python division.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+intdiv = a // b
+print(intdiv)
+
+floatdiv = a / b
+print(floatdiv)
