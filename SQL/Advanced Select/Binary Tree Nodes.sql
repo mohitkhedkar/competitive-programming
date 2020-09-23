8483d53e0e688c787d374044a256721904ae7db3
mohitkhedkar <mohitkhedkar521@gmail.com>
2020-09-23 21:59:32 +0530

    Advance Select added

---
+++ SQL/Advanced Select/Binary Tree Nodes.sql
@@ -0,0 +1,12 @@
+/*
+Enter your query here.
+*/
+
+SELECT N,
+    CASE
+        WHEN P IS NULL THEN 'Root'
+        WHEN N IN (SELECT P FROM BST) THEN 'Inner'
+        ELSE 'Leaf'
+    END
+FROM BST
+ORDER BY N;
