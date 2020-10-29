33a72d9df27e0be906ce78fd7f0c26dbc5267b1e
mohitkhedkar <mohitkhedkar521@gmail.com>
2020-10-29 17:26:50 +0530

    Basic added

---
+++ GeeksforGeeks/Basic/C++ Hello World.cpp
@@ -0,0 +1,26 @@
+// { Driver Code Starts
+
+
+
+#include<iostream>
+using namespace std;
+
+void helloWorld();
+
+int main()
+{
+    helloWorld();
+    cout<<endl;
+    return 0;
+}// } Driver Code Ends
+
+
+
+
+void helloWorld()
+{
+    
+    cout<<"Hello World";
+    //Type below cout<<"Hello World"; 
+    //cout will output to console
+}
