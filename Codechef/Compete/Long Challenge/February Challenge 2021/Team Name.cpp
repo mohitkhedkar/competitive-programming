a4722f5740e1ff13bd0bbf96dcbc193c0992cb6e
mohitkhedkar <mohitkhedkar521@gmail.com>
2021-02-10 21:07:15 +0530

    :tada: Team Name

--- Codechef/Compete/Long Challenge/February Challenge 2021/Team Name.cpp
+++ Codechef/Compete/Long Challenge/February Challenge 2021/Team Name.cpp
@@ -1,42 +1,52 @@
-#include <bits/stdc++.h>
-#define ll long long int
+#include<bits/stdc++.h>
+#include<string>
 using namespace std;
 
-int main() {
-    // your code goes here
-	int t;
-	cin>>t;
-	while(t--){
-	    ll n;
-	    cin>>n;
-	    string s[n];
-	    for(ll i=0;i<=n-1;i++){
-	        cin>>s[i];
-	    }
-	    
-	    sort(s,s+n);
-	    
-	    string p,q;
-	    ll ans=0;
-	    
-	    for(ll i=0;i<n-1;i++){
-	        for(ll j=i+1;j<n;j++){
-	            p=s[i];
-	            q=s[j];
-	            
-	            swap(p[0],q[0]);
-	            
-	            if(p[0]==q[0] || p == q ){
-	                
-	            } else if((!(binary_search(s,s+n,p))) && (!binary_search(s,s+n,q))){
-	                ans +=2;
-	            }
-	        }
-	    }
-	    
-	    cout<<ans<<endl;
-	    
-	}
-	
-	return 0;
+int common_elems(vector<char>v1,vector<char>v2){
+    set<char> s(v1.begin(),v1.end());
+    int ans=0;
+    for(auto x:v2){
+        if(s.find(x)!=s.end()){
+            ans++;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    int t;
+    cin>>t;    
+    
+    while(t--){
+        int n;  
+        cin>>n;
+        map<string,vector<char> >m;
+        map<string,vector<char> > :: iterator i1,j1;
+
+        string s;
+        for(int i=0;i<n;i++){
+            cin>>s;
+            if(s.length()>0){
+                m[s.substr(1)].push_back(s[0]);
+
+            }
+
+        }
+        int ans=0;
+        for(auto i : m){
+            for(auto j : m){
+                if(i.first!=j.first){
+                    int c=common_elems(i.second,j.second);
+                    ans+=(i.second.size()-c)*(j.second.size()-c);
+                }
+            }
+        }
+
+
+        cout<<ans<<endl;
+   
+    }
+
+
+    return 0;
 }
