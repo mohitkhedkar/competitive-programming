784935eecf435650905c749b98736f3fbed60613
mohitkhedkar <mohitkhedkar521@gmail.com>
2021-03-25 17:01:37 +0530

    :hammer: fork python Update

---
+++ GeeksforGeeks/Easy/Python/ Modules .py
@@ -0,0 +1,24 @@
+#{ 
+#Driver Code Starts
+#Initial Template for Python 3
+
+ # } Driver Code Ends
+#User function Template for python3
+
+import math
+def math_func():
+    #import math module and print pi and e
+    print("pi =",math.pi)
+    print("e =",math.e)
+    
+
+#{ 
+#Driver Code Starts.
+
+def main():
+    math_func()
+    
+    
+if __name__ == '__main__':
+    main()
+#} Driver Code Ends
