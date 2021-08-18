40e305598b4b2cade5afa8a50829f9aa1aaf89a2
mohitkhedkar <mohitkhedkar521@gmail.com>
2021-08-18 13:35:21 +0530

    :sparkles: Cpp

---
+++ InterView-Bit/Fast Track Courses/C++/Polymorphism.cpp
@@ -0,0 +1,28 @@
+/*
+#include<iostream>
+using namespace std;
+*/
+
+// YOUR CODE GOES HERE
+int compute(int num1,int num2){
+    if(num1>num2) return num1-num2;
+    else return num2-num1;
+
+}
+
+int compute(int num1){
+    return num1*num1;
+
+}
+
+/*
+int main()  {
+    int x,y;
+    cin>>x>>y;
+    cout<<compute(x,y)<<endl;
+    int z;
+    cin>>z;
+    cout<<compute(z)<<endl;
+    return 0;
+}
+*/
