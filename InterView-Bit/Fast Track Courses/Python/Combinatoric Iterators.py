6983d375c5ef06e096dd6562588e7aa7ec6c6e73
mohitkhedkar <mohitkhedkar521@gmail.com>
2021-08-20 17:47:57 +0530

    :sparkles: interviewbit python problems

---
+++ InterView-Bit/Fast Track Courses/Python/Combinatoric Iterators.py
@@ -0,0 +1,17 @@
+from itertools import permutations
+
+def main():
+    # YOUR CODE GOES HERE
+    # Please take input and print output to standard input/output (stdin/stdout)
+    # E.g. 'input()/raw_input()' for input & 'print' for output
+    S,K = input().split()
+    
+    for i in permutations(sorted(S),int(K)):
+        print("".join(i))
+
+
+
+    return 0
+
+if __name__ == '__main__':
+    main()
